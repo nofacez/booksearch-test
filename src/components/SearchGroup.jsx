8b7f1ad39b0a7446ec4497f423b12abaf99ee302
nofacez <genizzz@yandex.ru>
2021-06-04 11:04:30 +0300

    search group added

--- src/components/SearchGroup.jsx
+++ src/components/SearchGroup.jsx
@@ -1,42 +1,72 @@
+/* eslint-disable camelcase */
 /* eslint-disable consistent-return */
 import React, { useState, useEffect, useRef } from 'react';
+import { useDispatch } from 'react-redux';
 import axios from 'axios';
 
+import { getBooks, setLoading } from '../slices/booksSlice.js';
 import routes from '../routes.js';
 
 const SearchGroup = () => {
-  const [title, setTitle] = useState('');
+  const [value, setValue] = useState('');
   const isFirstRender = useRef(true);
+  const dispatch = useDispatch();
+  const { CancelToken } = axios;
+  const source = CancelToken.source();
 
   const getBooksList = async (bookTitle) => {
     try {
-      const response = await axios.get(routes.searchRoute(bookTitle));
-      console.log(response);
-      return response;
+      const response = await axios.get(routes.searchRoute(bookTitle),
+        {
+          cancelToken: source.token,
+          maxContentLength: 1000,
+        });
+      const bookslist = response.data.docs;
+      const filtered = bookslist.map((book) => {
+        const {
+          title, cover_i, author_name, publish_year, publisher, isbn,
+        } = book;
+        return {
+          title,
+          coverId: cover_i,
+          author: author_name,
+          date: publish_year,
+          publisher,
+          isbn,
+        };
+      });
+      console.log(filtered);
+      dispatch(getBooks(filtered));
+      dispatch(setLoading(false));
     } catch (e) {
       console.error(e);
     }
   };
 
   useEffect(() => {
-    if (isFirstRender.current) {
+    if (isFirstRender.current || value.length === 0) {
       isFirstRender.current = false;
     } else {
       const timeoutSearch = setTimeout(() => {
-        getBooksList(title);
+        dispatch(setLoading(true));
+        getBooksList(value);
       }, 1000);
-      return () => clearTimeout(timeoutSearch);
+      return () => {
+        source.cancel('The request has been cancelled');
+        clearTimeout(timeoutSearch);
+        dispatch(setLoading(false));
+      };
     }
-  }, [title]);
+  }, [value]);
 
   const handleChange = (e) => {
-    setTitle(e.target.value);
+    setValue(e.target.value);
   };
 
   return (
     <div className="search-group">
       <form className="d-flex">
-        <input type="text" className="search-input" value={title} onChange={handleChange} required />
+        <input type="text" className="search-input" value={value} onChange={handleChange} required placeholder="Enter a book title..." />
         <button type="submit" className="search-button">Send</button>
       </form>
     </div>
