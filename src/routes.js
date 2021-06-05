6a6a4f9d58f31f69ba4eec3cbb091dfb455d6366
nofacez <genizzz@yandex.ru>
2021-06-05 11:43:12 +0300

    updated routes

--- src/routes.js
+++ src/routes.js
@@ -3,7 +3,8 @@ const routes = {
     const newUrl = title.trim().split(' ').join('+');
     return `http://openlibrary.org/search.json?title=${newUrl}&limit=20`;
   },
-  smallCoverRoute: (id) => (id ? `http://covers.openlibrary.org/b/id/${id}-S.jpg` : 'https://banner2.cleanpng.com/20171217/82d/question-mark-png-5a36993e664362.4305931315135276144189.jpg'),
+  smallCoverRoute: (id) => `http://covers.openlibrary.org/b/id/${id}-S.jpg`,
+  mediumCoverRoute: (id) => `http://covers.openlibrary.org/b/id/${id}-M.jpg`,
 };
 
 export default routes;
