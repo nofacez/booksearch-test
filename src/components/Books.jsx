aed2174478128cd9912d5f77224128a15ea25ff2
nofacez <genizzz@yandex.ru>
2021-06-06 20:45:26 +0300

    added network issue handler

--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -6,38 +6,48 @@ import { BounceLoader } from 'react-spinners';
 import BookSnippet from './BookSnippet.jsx';
 
 const Books = () => {
+  const { books, status } = useSelector((state) => state.booksList);
   const Spinner = () => (
     <div className="spinner-container">
       <div className="spinner">
         <span className="sr-only">Loading...</span>
         <BounceLoader size={50} />
       </div>
-      <p className="spinner-text">Browsing the shelves...</p>
+      <p className="spinner-text mt-35">Browsing the shelves...</p>
     </div>
   );
   const NotFound = () => (
     <div className="not-found">
-      <p>We couldn&apos;t find anything</p>
-      <p>That&apos;s what happens if you don&apos;t return books to library</p>
+      <p className="mt-35">We couldn&apos;t find anything</p>
+      <p>That&apos;s what happens if you don&apos;t return books to the library</p>
     </div>
   );
-  const { books, loading, arrayisEmpty } = useSelector((state) => state.booksList);
+  const NetworkIssue = () => (
+    <div className="network-issue">
+      <p className="mt-35">Oops</p>
+      <p>We couldn&apos;t reach the Web :(</p>
+    </div>
+  );
 
-  if (loading) {
+  if (status === 'loading') {
     return <Spinner />;
+  }
+
+  if (status === 'noData') {
+    return <NotFound />;
+  }
+
+  if (status === 'networkIssue') {
+    return <NetworkIssue />;
   }
 
   return (
     <div className="section">
-      { arrayisEmpty
-        ? <NotFound />
-        : (
-          <ul className="books-list">
-            { books.map(({
-              coverId, title, author, key,
-            }) => (<BookSnippet coverId={coverId} title={title} author={author} id={key} key={uniqueId()} />))}
-          </ul>
-        )}
+      <ul className="books-list">
+        { books.map(({
+          coverId, title, author, key,
+        }) => (<BookSnippet coverId={coverId} title={title} author={author} id={key} key={uniqueId()} />))}
+      </ul>
     </div>
   );
 };
