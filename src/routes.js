4782a81836e52d95ce9214e1655d19bf9a8b0144
nofacez <genizzz@yandex.ru>
2021-06-28 11:07:37 +0300

    added s to http

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,10 @@
 const routes = {
   searchRoute: (title) => {
     const newUrl = title.trim().split(' ').join('+');
-    return `http://openlibrary.org/search.json?title=${newUrl}&limit=20`;
+    return `https://openlibrary.org/search.json?title=${newUrl}&limit=20`;
   },
-  smallCoverRoute: (id) => `http://covers.openlibrary.org/b/id/${id}-S.jpg`,
-  mediumCoverRoute: (id) => `http://covers.openlibrary.org/b/id/${id}-M.jpg`,
+  smallCoverRoute: (id) => `https://covers.openlibrary.org/b/id/${id}-S.jpg`,
+  mediumCoverRoute: (id) => `https://covers.openlibrary.org/b/id/${id}-M.jpg`,
 };
 
 export default routes;
